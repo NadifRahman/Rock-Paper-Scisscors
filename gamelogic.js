77512a7fac0082b39e9751af1a53f7e04957f928
NadifRahman <nadif.rah@gmail.com>
2024-04-27 11:31:35 -0400

    Add game logic + refined UI

--- gamelogic.js
+++ gamelogic.js
@@ -1,96 +1,123 @@
+let humanScoreDisplay = document.querySelector('.score.human');
+let computerScoreDisplay = document.querySelector('.score.computer');
+let humanIcon = document.querySelector(`.human img`);
+let computerIcon = document.querySelector(`.computer img`);
+
+let rockBtn = document.querySelector(`#rock-button`);
+let paperBtn = document.querySelector(`#paper-button`);
+let scisscorBtn = document.querySelector(`#scisscor-button`);
+let resetBtn = document.querySelector(`#reset`);
+let statusDisplay = document.querySelector(`.status`);
+let body = document.querySelector(`body`);
+
+let humanScore = 0;
+let computerScore = 0;
 //effects: returns a string, either "rock," ",paper" or "scisscors", which is randomly chosen
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3); //generate a random number from 0 to 2
    
     switch (random) { //returns based on rng
         case 0:
+            computerIcon.src = `../images/fist.png`;
             return "rock";
         case 1:
+            computerIcon.src = `../images/hand-palm.png`;
             return "paper";
         case 2:
+            computerIcon.src = `../images/swipe.png`;
             return "scisscors";
         default:
+            computerIcon.src = `../images/fist.png`;
             return "rock";
     }
 }
 
-//effects: returns a string, either "rock," ",paper" or "scisscors" which is obtained from user
-function getHumanChoice() {
-    userInput = prompt("Please enter: rock, paper, or scisscors", "rock"); 
-    userInput = userInput.toLowerCase(); //make the input only lowercase, code can break if user fails to enter string
-    if (userInput !== "rock" && userInput !== "paper" && userInput !== "scisscors") { console.log("Please enter correct options only."); return; }
-    return userInput;
-}
-//modifies: humanScore, computerScore (both are strings)
-//effects: 
 
-
-function playGame() {
-
-    function playRound(humanChoice, computerChoice) {
-        if(humanChoice === "rock") { //if user entered rock
-            if(computerChoice === "rock") {
-                console.log("Rock vs. Rock. Tie game. No one wins.");
-            }
-            else if (computerChoice === "paper"){
-                console.log("Rock vs. Paper. Computer wins.");
-                computerScore++; //increment computer's score
-            }
-            else { //if user entered scisscors
-                console.log("Rock vs. Scisscors. Human wins.");
-                humanScore++; //increment computer's score
-            }
-        }
-        else if (humanChoice === "paper") { //if user entered paper
-            if(computerChoice === "rock") {
-                console.log("Paper vs. Rock. Human wins.");
-                humanScore++;
-            }
-            else if (computerChoice === "paper"){
-                console.log("Paper vs. Paper. Tie game. No one wins.");
-            }
-            else { //if user entered scisscors
-                console.log("Paper vs. Scisscors. Computer wins.");
-                computerScore++; //increment computer's score
-            }
-        }
-        else { //if user entered scisscor
-        
-            if(computerChoice === "rock") {
-                console.log("Scisscor vs. Rock. Computer wins.");
-                computerScore++;
-            }
-            else if (computerChoice === "paper"){
-                console.log("Scisscor vs. Paper. Human wins.");
-                humanScore++;
-            }
-            else { //if user entered scisscors
-                console.log("Scisscor vs. Scisscors. Tie game. No one wins.");
-            }
-        }
+function playRound(humanChoice, computerChoice) { 
     
+    if(humanChoice === "rock") { //if user entered rock
+        if(computerChoice === "rock") {
+            statusDisplay.textContent = "Rock vs. Rock. Tie game. No one wins.";
+        }
+        else if (computerChoice === "paper"){
+            statusDisplay.textContent = "Rock vs. Paper. Computer wins.";
+            computerScore++; //increment computer's score
+        }
+        else { //if user entered scisscors
+            statusDisplay.textContent = "Rock vs. Scisscors. Human wins.";
+            humanScore++; //increment computer's score
+        }
+    }
+    else if (humanChoice === "paper") { //if user entered paper
+        if(computerChoice === "rock") {
+            statusDisplay.textContent = "Paper vs. Rock. Human wins.";
+            humanScore++;
+        }
+        else if (computerChoice === "paper"){
+            statusDisplay.textContent = "Paper vs. Paper. Tie game. No one wins.";
+          
+        }
+        else { //if user entered scisscors
+            statusDisplay.textContent = "Paper vs. Scisscors. Computer wins.";
+            computerScore++; //increment computer's score
+        }
+    }
+    else { //if user entered scisscor
+    
+        if(computerChoice === "rock") {
+            statusDisplay.textContent = "Scisscor vs. Rock. Computer wins.";
+            computerScore++;
+        }
+        else if (computerChoice === "paper"){
+            statusDisplay.textContent = "Scisscor vs. Paper. Human wins.";
+            humanScore++;
+        }
+        else { //if user entered scisscors
+            statusDisplay.textContent = "Scisscor vs. Scisscors. Tie game. No one wins.";
+        }
     }
 
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
+    humanScoreDisplay.textContent = humanScore; //sets the score display
+    computerScoreDisplay.textContent = computerScore;
+
+    if(humanScore === 5) {
+        statusDisplay.textContent = "Human is the winner! Bravo.";
     }
-    if(humanScore > computerScore) {
-        console.log(`Human beat computer with ${humanScore} points over ${computerScore}.`);
+    else if (computerScore === 5) {
+        statusDisplay.textContent = "Computer is the winner! Better luck next time!";
     }
-    else if (humanScore === computerScore) {
-        console.log(`Tie game, both have ${humanScore} points.`);
-    }
-    else { //if computer score has higher amount of points
-        console.log(`Computer beat human with ${computerScore} points over ${humanScore}.`);
-    }
+    console.log(humanScore);
+    console.log(computerScore);
 
 }
 
-playGame();
+rockBtn.addEventListener(`click`, () => {
+    if(humanScore === 5 || computerScore === 5) return; //if any scores are 5, then no need to play round so return
+    humanIcon.src = `../images/fist.png`;
+    playRound('rock', getComputerChoice());
+});
 
+paperBtn.addEventListener('click', () => {
+    if(humanScore === 5 || computerScore === 5) return; 
+    humanIcon.src = `../images/hand-palm.png`;
+    playRound('paper', getComputerChoice());
+});
 
+scisscorBtn.addEventListener(`click`, () => {
+    if(humanScore === 5 || computerScore === 5) return; 
+    humanIcon.src = `../images/swipe.png`;
+    playRound('scisscors', getComputerChoice());
+});
+
+resetBtn.addEventListener('click', () => {
+    humanScore = 0;
+    computerScore = 0;
+    humanScoreDisplay.textContent = humanScore;
+    computerScoreDisplay.textContent = computerScore;
+    humanIcon.src = `../images/questionmark.png`;
+    computerIcon.src = `../images/questionmark.png`;
+    statusDisplay.textContent = "New game."
+}) 
 
 
 
